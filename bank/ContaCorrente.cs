38cb6de51474a365583af6ee4e7ab0eb93eaf269
Johnny Anacleto <jhonny.anacleto@gruposbf.com.br>
2021-05-04 17:58:10 -0300

    Inserindo alterações da aula 02

---
+++ bank/ContaCorrente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bank
+{
+    class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int conta;
+        public double saldo;
+    }
+}
--- bank/Program.cs
+++ bank/Program.cs
@@ -6,13 +6,29 @@ namespace bank
     {
         static void Main(string[] args)
         {
-            public string titular = "Debiny";
-            public int agencia = 123;
-            public int conta = 123456;
-            public double saldo = 0.00;
+            ContaCorrente cc = new ContaCorrente();
+            cc.titular = "Jhonny";
+            cc.agencia = 1234;
+            cc.conta = 37163;
+            cc.saldo = 500;
 
+            ContaCorrente cc2 = new ContaCorrente();
+            cc2.titular = "Debiny";
+            cc2.agencia = 4321;
+            cc2.conta = 18372;
+            cc2.saldo = 800;
 
-            Console.ReadLine();
+            
+            Console.WriteLine("Nome do titular da primeira conta: " + cc.titular);
+            Console.WriteLine("Número da agência: " + cc.agencia);
+            Console.WriteLine("Número da conta: " + cc.conta);
+            Console.WriteLine("Saldo da conta é: " + cc.saldo);
+
+            Console.WriteLine("Nome do titular da segunda conta: " + cc2.titular);
+            Console.WriteLine("Número da agência: " + cc2.agencia);
+            Console.WriteLine("Número da conta: " + cc2.conta);
+            Console.WriteLine("Saldo da conta é: " + cc2.saldo);
+
         }
     }
 }
