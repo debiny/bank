0c2b6a4acbcf71da145b17f82ad968084974e3b7
Debiny <debiny.rocha@gruposbf.com.br>
2021-05-20 18:12:26 -0300

    aula 3

---
+++ bank/ComercialPartner.cs
@@ -0,0 +1,20 @@
+﻿using bank.Sistemas;
+
+namespace bank
+{
+    public class ComercialPartner : IAuthentication
+    {
+        public string password { get; set; }
+
+        public bool Authentication(string pass)
+        {
+            return this.password == pass;
+        }
+
+        public bool Authentication(string pass, string name)
+        {
+            return this.password == pass;
+        }
+
+    }
+}
---
+++ bank/Sistemas/IAuthentication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bank.Sistemas
+{
+    public interface IAuthentication
+    {
+        bool Authentication(string pass);
+    }
+}
